ab1a36593a38cd0fd818e984433cbfa464515ea6
Arina Trofimova <arinatrofimova02@mail.ru>
2024-11-16 19:08:47 +0400

    innitial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+	const [value, setValue] = useState(0);
+	const [colour, setColour] = useState('');
+
+	const plusValue = () => {
+		const newValue = value + 1;
+
+		if (newValue > 13){
+			setColour('warm');
+		} else if (newValue > 26 ){
+			setColour('hot');
+		} else if (newValue < 0) {
+			setColour('cold');
+		} else if (newValue >=0 && newValue < 13) {
+			setColour('');
+		}
+
+		setValue(newValue);
+	}
+
+	const minusValue = () => {
+		const newValue = value - 1;
+
+		if (newValue > 13 && newValue < 26){
+			setColour('warm');
+		} else if (newValue > 26 ){
+			setColour('hot');
+		} else if (newValue < 0) {
+			setColour('cold');
+		} else if (newValue >=0 && newValue < 13) {
+			setColour('');
+		}
+
+		setValue(newValue);
+	}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+			<div className='box'>
+				{/* <div className='temp-value-box'> */}
+					<div className={`temp-value ${colour}`}>
+						{value}°C
+					</div>
+				{/* </div> */}
+				
+				<div className='button-box'>
+					<button onClick={() => plusValue()}>+</button>
+					<button onClick={() => minusValue()}>-</button>
+				</div>
+			</div>
     </div>
   );
 }
